72d00a6a5de6ec5fcf018ba606694e62f93d8d29
elizabeth2401 <79124704+elizabeth2401@users.noreply.github.com>
2021-06-04 01:57:04 -0500

    Update OpcProveedores.java

--- OpcProveedores.java
+++ OpcProveedores.java
@@ -7,11 +7,11 @@ public class OpcProveedores extends JFrame implements ActionListener{
    public JPanel OPPanel;
    public JTextField nombre,busqueda,idProveedor,tel;
    public JButton btnIdP, btnConsultaG, btnVolver, btnBuscar, btnBuscarE, btnGuardar,btnGuardarM, btnEliminar, btnCancelar, btnBusquedaM;
-   public String menuTservicio[]={"","Electrico"};
+   public String menuTservicio[] = {"","Electrico"};
    public JComboBox tServicio;
    public JLabel texto;
    public OpcProveedores(int opc){
-      opcion=opc;
+      opcion = opc;
       setResizable(false);
       this.setSize(720, 480);
       setLocationRelativeTo(null);
@@ -47,56 +47,56 @@ public class OpcProveedores extends JFrame implements ActionListener{
       OPPanel.add(imagenProveedor);
 
       
-      JLabel telT= new JLabel("Numero Telefonico");
+      JLabel telT = new JLabel("Numero Telefonico");
       telT.setBounds(50, 95, 150, 30);
       tel = new JTextField();
       tel.setBounds(50, 120, 200, 30);
       OPPanel.add(tel);
       OPPanel.add(telT);
       
-      JLabel nombreT= new JLabel("Nombre");
+      JLabel nombreT = new JLabel("Nombre");
       nombreT.setBounds(50, 155, 150, 30);
       nombre = new JTextField();
       nombre.setBounds(50, 180, 200, 30);
       OPPanel.add(nombre);
       OPPanel.add(nombreT);
             
-      JLabel tServicioT= new JLabel("Tipo de Servicio");
+      JLabel tServicioT = new JLabel("Tipo de Servicio");
       tServicioT.setBounds(50, 215, 150, 30);
       tServicio = new JComboBox(menuTservicio);
       tServicio.setBounds(50, 240, 200, 30);
       OPPanel.add(tServicioT);
       OPPanel.add(tServicio);
       
-      btnGuardar= new JButton("Guardar");
+      btnGuardar = new JButton("Guardar");
       btnGuardar.setBounds(50, 360, 80, 30);
       btnGuardar.addActionListener(this);
       OPPanel.add(btnGuardar);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(150, 360, 80, 30);
       btnVolver.addActionListener(this);
       OPPanel.add(btnVolver);
    }
    public void screenConsultar(){
    
-      texto= new JLabel();
+      texto = new JLabel();
       setTitle("Consultar Proveedor");
       texto.setText("Opciones de busqueda:");
       texto.setBounds(80, 5, 300, 30);
       OPPanel.add(texto);
       
-      btnIdP= new JButton("Busqueda por Id");
+      btnIdP = new JButton("Busqueda por Id");
       btnIdP.setBounds(130, 45, 200, 30);
       btnIdP.addActionListener(this);
       OPPanel.add(btnIdP);
             
-      btnConsultaG= new JButton("Consulta General");
+      btnConsultaG = new JButton("Consulta General");
       btnConsultaG.setBounds(340, 45, 200, 30);
       btnConsultaG.addActionListener(this);
       OPPanel.add(btnConsultaG);
       
-      btnBuscar= new JButton("Buscar");
+      btnBuscar = new JButton("Buscar");
       btnBuscar.setBounds(500, 100, 100, 30);
       btnBuscar.setVisible(false);
       btnBuscar.addActionListener(this);
@@ -107,7 +107,7 @@ public class OpcProveedores extends JFrame implements ActionListener{
       busqueda.setVisible(false);
       OPPanel.add(busqueda);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(50, 360, 100, 30);
       btnVolver.addActionListener(this);
       OPPanel.add(btnVolver);
@@ -115,12 +115,12 @@ public class OpcProveedores extends JFrame implements ActionListener{
    public void screenModificar(){
       setTitle("Modificar Proveedor");
       
-      texto= new JLabel();
+      texto = new JLabel();
       texto.setText("Ingrese Id de Proveedor:");
       texto.setBounds(90, 35, 300, 30);
       OPPanel.add(texto);
       
-      btnBusquedaM= new JButton("Buscar");
+      btnBusquedaM = new JButton("Buscar");
       btnBusquedaM.setBounds(500, 60, 100, 30);
       btnBusquedaM.addActionListener(this);
       OPPanel.add(btnBusquedaM);
@@ -135,20 +135,20 @@ public class OpcProveedores extends JFrame implements ActionListener{
       imagenProveedor.setIcon(new ImageIcon(imagenF.getImage().getScaledInstance(340, 340, Image.SCALE_SMOOTH)));
       OPPanel.add(imagenProveedor);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(150, 360, 80, 30);
       btnVolver.addActionListener(this);
       OPPanel.add(btnVolver);
          
-      JLabel telT= new JLabel("Numero Telefonico");
+      JLabel telT = new JLabel("Numero Telefonico");
       telT.setBounds(50, 95, 200, 30);
-      tel= new JTextField();
+      tel = new JTextField();
       tel.setBounds(50, 120, 200, 30);
       tel.setEnabled(false);
       OPPanel.add(tel);
       OPPanel.add(telT);
          
-      JLabel nombreT= new JLabel("Nombre");
+      JLabel nombreT = new JLabel("Nombre");
       nombreT.setBounds(50, 155, 200, 30);
       nombre = new JTextField();
       nombre.setBounds(50, 180, 200, 30);
@@ -156,7 +156,7 @@ public class OpcProveedores extends JFrame implements ActionListener{
       OPPanel.add(nombre);
       OPPanel.add(nombreT);
                
-      JLabel tServicioT= new JLabel("Tipo de Servicio");
+      JLabel tServicioT = new JLabel("Tipo de Servicio");
       tServicioT.setBounds(50, 215, 200, 30);
       tServicio = new JComboBox(menuTservicio);
       tServicio.setBounds(50, 240, 200, 30);
@@ -164,7 +164,7 @@ public class OpcProveedores extends JFrame implements ActionListener{
       OPPanel.add(tServicioT);
       OPPanel.add(tServicio);
       
-      btnGuardarM= new JButton("Guardar");
+      btnGuardarM = new JButton("Guardar");
       btnGuardarM.setBounds(50, 360, 80, 30);
       btnGuardarM.addActionListener(this);
       btnGuardarM.setEnabled(false);
@@ -173,12 +173,12 @@ public class OpcProveedores extends JFrame implements ActionListener{
    public void screenEliminar(){
       setTitle("Eliminar Proveedor");
       
-      texto= new JLabel();
+      texto = new JLabel();
       texto.setText("Ingrese Id de Proveedor:");
       texto.setBounds(90, 35, 300, 30);
       OPPanel.add(texto);
       
-      btnBuscarE= new JButton("Buscar");
+      btnBuscarE = new JButton("Buscar");
       btnBuscarE.setBounds(500, 60, 100, 30);
       btnBuscarE.addActionListener(this);
       OPPanel.add(btnBuscarE);
@@ -187,19 +187,19 @@ public class OpcProveedores extends JFrame implements ActionListener{
       busqueda.setBounds(90, 60, 400, 30);
       OPPanel.add(busqueda);
       
-      btnEliminar= new JButton("Eliminar");
+      btnEliminar = new JButton("Eliminar");
       btnEliminar.setBounds(280, 360, 100, 30);
       btnEliminar.addActionListener(this);
       btnEliminar.setVisible(false);
       OPPanel.add(btnEliminar);
       
-      btnCancelar= new JButton("Cancelar");
+      btnCancelar = new JButton("Cancelar");
       btnCancelar.setBounds(390, 360, 100, 30);
       btnCancelar.addActionListener(this);
       btnCancelar.setVisible(false);
       OPPanel.add(btnCancelar);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(50, 360, 100, 30);
       btnVolver.addActionListener(this);
       OPPanel.add(btnVolver);
