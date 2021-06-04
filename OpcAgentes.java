35783eff0263c257db65520dec66b7a497c3e582
elizabeth2401 <79124704+elizabeth2401@users.noreply.github.com>
2021-06-04 01:41:18 -0500

    Update OpcAgentes.java

--- OpcAgentes.java
+++ OpcAgentes.java
@@ -7,11 +7,11 @@ public class OpcAgentes extends JFrame implements ActionListener{
    public JPanel OAPanel;
    public JTextField nombre,busqueda,tel,email,cargo,idAgente;
    public JButton btnIdP, btnConsultaG, btnVolver, btnBuscar, btnBuscarE, btnGuardar,btnGuardarM, btnEliminar, btnCancelar, btnBusquedaM;
-   public String menuIdP[]={"","P1"};
+   public String menuIdP[] = {"","P1"};
    public JComboBox idProveedor;
    public JLabel texto;
    public OpcAgentes(int opc){
-      opcion=opc;
+      opcion = opc;
       setResizable(false);
       this.setSize(720, 480);
       setLocationRelativeTo(null);
@@ -46,7 +46,7 @@ public class OpcAgentes extends JFrame implements ActionListener{
       imagenAgente.setIcon(new ImageIcon(imagenF.getImage().getScaledInstance(340, 340, Image.SCALE_SMOOTH)));
       OAPanel.add(imagenAgente);
       
-      JLabel idProveedorT= new JLabel("Proveedor");
+      JLabel idProveedorT = new JLabel("Proveedor");
       idProveedorT.setBounds(50,25,150,30);
       idProveedor = new JComboBox(menuIdP);
       idProveedor.setBounds(50, 50, 200,30);
@@ -60,56 +60,56 @@ public class OpcAgentes extends JFrame implements ActionListener{
       OAPanel.add(nombre);
       OAPanel.add(nombreT);
       
-      JLabel telT= new JLabel("Numero Telefonico");
+      JLabel telT = new JLabel("Numero Telefonico");
       telT.setBounds(50, 125, 150, 30);
       tel = new JTextField();
       tel.setBounds(50, 150, 200, 30);
       OAPanel.add(tel);
       OAPanel.add(telT);
       
-      JLabel emailT= new JLabel("Email");
+      JLabel emailT = new JLabel("Email");
       emailT.setBounds(50, 175, 150, 30);
       email = new JTextField();
       email.setBounds(50, 200, 200, 30);
       OAPanel.add(email);
       OAPanel.add(emailT);
             
-      JLabel cargoT= new JLabel("Cargo");
+      JLabel cargoT = new JLabel("Cargo");
       cargoT.setBounds(50, 225, 150, 30);
       cargo = new JTextField();
       cargo.setBounds(50, 250, 200, 30);
       OAPanel.add(cargoT);
       OAPanel.add(cargo);
       
-      btnGuardar= new JButton("Guardar");
+      btnGuardar = new JButton("Guardar");
       btnGuardar.setBounds(50, 360, 80, 30);
       btnGuardar.addActionListener(this);
       OAPanel.add(btnGuardar);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(150, 360, 80, 30);
       btnVolver.addActionListener(this);
       OAPanel.add(btnVolver);
    }
    public void screenConsultar(){
    
-      texto= new JLabel();
+      texto = new JLabel();
       setTitle("Consultar Agente");
       texto.setText("Opciones de busqueda:");
       texto.setBounds(80, 5, 300, 30);
       OAPanel.add(texto);
       
-      btnIdP= new JButton("Busqueda por Id");
+      btnIdP = new JButton("Busqueda por Id");
       btnIdP.setBounds(130, 45, 200, 30);
       btnIdP.addActionListener(this);
       OAPanel.add(btnIdP);
             
-      btnConsultaG= new JButton("Consulta General");
+      btnConsultaG = new JButton("Consulta General");
       btnConsultaG.setBounds(340, 45, 200, 30);
       btnConsultaG.addActionListener(this);
       OAPanel.add(btnConsultaG);
       
-      btnBuscar= new JButton("Buscar");
+      btnBuscar = new JButton("Buscar");
       btnBuscar.setBounds(500, 100, 100, 30);
       btnBuscar.setVisible(false);
       btnBuscar.addActionListener(this);
@@ -120,7 +120,7 @@ public class OpcAgentes extends JFrame implements ActionListener{
       busqueda.setVisible(false);
       OAPanel.add(busqueda);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(50, 360, 100, 30);
       btnVolver.addActionListener(this);
       OAPanel.add(btnVolver);
@@ -128,7 +128,7 @@ public class OpcAgentes extends JFrame implements ActionListener{
    public void screenModificar(){
       setTitle("Modificar Agente");
       
-      texto= new JLabel();
+      texto = new JLabel();
       texto.setText("Ingrese Id de Agente:");
       texto.setBounds(90, 15, 300, 30);
       OAPanel.add(texto);
@@ -148,12 +148,12 @@ public class OpcAgentes extends JFrame implements ActionListener{
       imagenAgentes.setIcon(new ImageIcon(imagenF.getImage().getScaledInstance(340, 340, Image.SCALE_SMOOTH)));
       OAPanel.add(imagenAgentes);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(150, 360, 80, 30);
       btnVolver.addActionListener(this);
       OAPanel.add(btnVolver);
       
-      JLabel idProveedorT= new JLabel("Proveedor");
+      JLabel idProveedorT = new JLabel("Proveedor");
       idProveedorT.setBounds(50,75,150,30);
       idProveedor = new JComboBox(menuIdP);
       idProveedor.setBounds(50, 100, 200,30);
@@ -161,7 +161,7 @@ public class OpcAgentes extends JFrame implements ActionListener{
       OAPanel.add(idProveedorT);
       OAPanel.add(idProveedor);
    
-      JLabel nombreT= new JLabel("Nombre");
+      JLabel nombreT = new JLabel("Nombre");
       nombreT.setBounds(50, 125, 150, 30);
       nombre = new JTextField();
       nombre.setBounds(50, 150, 200, 30);
@@ -169,7 +169,7 @@ public class OpcAgentes extends JFrame implements ActionListener{
       OAPanel.add(nombre);
       OAPanel.add(nombreT);
       
-      JLabel telT= new JLabel("Numero Telefonico");
+      JLabel telT = new JLabel("Numero Telefonico");
       telT.setBounds(50, 175, 150, 30);
       tel = new JTextField();
       tel.setBounds(50, 200, 200, 30);
@@ -177,7 +177,7 @@ public class OpcAgentes extends JFrame implements ActionListener{
       OAPanel.add(tel);
       OAPanel.add(telT);
       
-      JLabel emailT= new JLabel("Email");
+      JLabel emailT = new JLabel("Email");
       emailT.setBounds(50, 225, 150, 30);
       email = new JTextField();
       email.setBounds(50, 250, 200, 30);
@@ -185,7 +185,7 @@ public class OpcAgentes extends JFrame implements ActionListener{
       OAPanel.add(email);
       OAPanel.add(emailT);
             
-      JLabel cargoT= new JLabel("Cargo");
+      JLabel cargoT = new JLabel("Cargo");
       cargoT.setBounds(50, 275, 150, 30);
       cargo = new JTextField();
       cargo.setBounds(50, 300, 200, 30);
@@ -202,12 +202,12 @@ public class OpcAgentes extends JFrame implements ActionListener{
    public void screenEliminar(){
       setTitle("Eliminar Agente");
       
-      texto= new JLabel();
+      texto = new JLabel();
       texto.setText("Ingrese Id de Agente:");
       texto.setBounds(90, 35, 300, 30);
       OAPanel.add(texto);
       
-      btnBuscarE= new JButton("Buscar");
+      btnBuscarE = new JButton("Buscar");
       btnBuscarE.setBounds(500, 60, 100, 30);
       btnBuscarE.addActionListener(this);
       OAPanel.add(btnBuscarE);
@@ -216,19 +216,19 @@ public class OpcAgentes extends JFrame implements ActionListener{
       busqueda.setBounds(90, 60, 400, 30);
       OAPanel.add(busqueda);
       
-      btnEliminar= new JButton("Eliminar");
+      btnEliminar = new JButton("Eliminar");
       btnEliminar.setBounds(280, 360, 100, 30);
       btnEliminar.addActionListener(this);
       btnEliminar.setVisible(false);
       OAPanel.add(btnEliminar);
       
-      btnCancelar= new JButton("Cancelar");
+      btnCancelar = new JButton("Cancelar");
       btnCancelar.setBounds(390, 360, 100, 30);
       btnCancelar.addActionListener(this);
       btnCancelar.setVisible(false);
       OAPanel.add(btnCancelar);
       
-      btnVolver= new JButton("Volver");
+      btnVolver = new JButton("Volver");
       btnVolver.setBounds(50, 360, 100, 30);
       btnVolver.addActionListener(this);
       OAPanel.add(btnVolver);
