376433cc5e51aa590ec80514247f4a98cf035c40
Pablo141 <69791016+Pablo141@users.noreply.github.com>
2021-06-08 22:29:50 -0500

    Delete KEVIN_VERSION1/TransportesJISA-master directory

--- KEVIN_VERSION1/TransportesJISA-master/Principal_TransportesJISA.java
+++
@@ -1,9 +0,0 @@
-/*
-llama a la ventana de login para poner el user y pass
-*/
-public class Principal_TransportesJISA{
-   public static void main(String args[]){
-      Login lg = new Login();
-      lg.setVisible(true);//Vuelve visible la ventana de login
-   }//main
-}//class
